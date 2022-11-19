284d0e4b5fefc2d0df597478cf9354ae99210df3
amozhdehi <amojdehi@gmail.com>
2022-11-18 17:12:46 -0700

    debug

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,8 +1,61 @@
 import pandas as pd
 # from utils.pre_processing import fix_imbalance_ratio
-from utils.pre_processing import fix_imbalance_ratio
+from utils.pre_processing import fix_imbalance_ratio, normalize, sparse_mx_to_torch_sparse_tensor, print_edges_num, fix_imbalance_ratio2
 import numpy as np
 import networkx as nx
+import torch
+import argparse
+import scipy.sparse as sp
+import numpy as np
+import torch
+import ipdb
+from scipy.io import loadmat
+import networkx as nx
+import multiprocessing as mp
+import torch.nn.functional as F
+from functools import partial
+import random
+from sklearn.metrics import roc_auc_score, f1_score
+from copy import deepcopy
+from scipy.spatial.distance import pdist,squareform
+
+def load_data(path="data/cora/", dataset="cora"):#modified from code: pygcn
+    """Load citation network dataset (cora only for now)"""
+    #input: idx_features_labels, adj
+    #idx,labels are not required to be processed in advance
+    #adj: save in the form of edges. idx1 idx2 
+    #output: adj, features, labels are all torch.tensor, in the dense form
+    #-------------------------------------------------------
+    print('Loading {} dataset...'.format(dataset))
+    idx_features_labels = np.genfromtxt("{}{}.content".format(path, dataset),
+                                        dtype=np.dtype(str))
+    features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
+    labels = idx_features_labels[:, -1]
+    set_labels = set(labels)
+    classes_dict = {c: np.arange(len(set_labels))[i] for i, c in enumerate(set_labels)}
+    classes_dict = {'Neural_Networks': 0, 'Reinforcement_Learning': 1, 'Probabilistic_Methods': 2, 'Case_Based': 3, 'Theory': 4, 'Rule_Learning': 5, 'Genetic_Algorithms': 6}
+    #ipdb.set_trace()
+    labels = np.array(list(map(classes_dict.get, labels)))
+    # build graph
+    idx = np.array(idx_features_labels[:, 0], dtype=np.int64)
+    idx_map = {j: i for i, j in enumerate(idx)}
+    edges_unordered = np.genfromtxt("{}{}.cites".format(path, dataset),
+                                    dtype=np.int64)
+    edges = np.array(list(map(idx_map.get, edges_unordered.flatten())),
+                     dtype=np.int32).reshape(edges_unordered.shape)
+    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
+                        shape=(labels.shape[0], labels.shape[0]),
+                        dtype=np.float32)
+    # build symmetric adjacency matrix
+    adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+    features = normalize(features)
+    features = torch.DoubleTensor(np.array(features.todense()))
+    labels = torch.LongTensor(labels)
+    print_edges_num(adj.todense(), labels)
+    adj = sparse_mx_to_torch_sparse_tensor(adj)
+    #adj = torch.FloatTensor(np.array(adj.todense()))
+    return adj, features, labels
+
 
 # Generate the Pima Indian Diabetes (Diabetes) graph through the method used in : https://arxiv.org/abs/2103.00221
 def data_loader_diabetes(args):
@@ -36,11 +89,17 @@ def data_loader_diabetes(args):
     # diabetes_train_idx = list(range(int(dataset_length * args.train_ratio)))
     # diabetes_val_idx = list(range(int(dataset_length * args.train_ratio), int(dataset_length * (1 - args.test_ratio))))
     # diabetes_test_idx = list(range(int(dataset_length * (1 - args.test_ratio)), dataset_length))
+
     diabetes_features_df, diabetes_labels_df, diabetes_train_idx, diabetes_val_idx ,diabetes_val_idx = fix_imbalance_ratio(args.imbalance_ratio, \
         diabetes_labels_df, diabetes_features_df, diabetes_train_idx, diabetes_val_idx, diabetes_test_idx)
+
+    # diabetes_features_df, diabetes_labels_df, diabetes_train_idx, diabetes_val_idx ,diabetes_val_idx = fix_imbalance_ratio2(args.imbalance_ratio, \
+    #     diabetes_labels_df, diabetes_features_df, diabetes_train_idx, diabetes_val_idx, diabetes_test_idx)
+
     G = nx.Graph()
     gam = 4
     # diabetes_df = [diabetes_labels_df, iabetes_features_df]
+    diabetes_features_df = normalize(diabetes_features_df)
     for patient_id, row_vals in diabetes_df.iterrows():
         # print(patient_id, row_vals)
         G.add_node(str(patient_id), 
@@ -126,10 +185,16 @@ def data_loader_haberman(args):
     # diabetes_train_idx = list(range(int(dataset_length * args.train_ratio)))
     # diabetes_val_idx = list(range(int(dataset_length * args.train_ratio), int(dataset_length * (1 - args.test_ratio))))
     # diabetes_test_idx = list(range(int(dataset_length * (1 - args.test_ratio)), dataset_length))
+
     haberman_features_df, haberman_labels_df, haberman_train_idx, haberman_val_idx ,haberman_val_idx = fix_imbalance_ratio(args.imbalance_ratio, \
         haberman_labels_df, haberman_features_df, haberman_train_idx, haberman_val_idx, haberman_test_idx)
+
+    # haberman_features_df, haberman_labels_df, haberman_train_idx, haberman_val_idx ,haberman_val_idx = fix_imbalance_ratio2(args.imbalance_ratio, \
+    #     haberman_labels_df, haberman_features_df, haberman_train_idx, haberman_val_idx, haberman_test_idx)
+
     G = nx.Graph()
     gam = 2
+    # haberman_features_df = normalize(haberman_features_df)
     for patient_id, row_vals in haberman_df.iterrows():
         G.add_node(str(patient_id),
             age = row_vals['Age'],
--- utils/pre_processing.py
+++ utils/pre_processing.py
@@ -12,6 +12,20 @@ def normalize(mx):
     mx = r_mat_inv.dot(mx)
     return mx
 
+def print_edges_num(dense_adj, labels):
+    c_num = labels.max().item()+1
+    dense_adj = np.array(dense_adj)
+    labels = np.array(labels)
+
+    for i in range(c_num):
+        for j in range(c_num):
+            #ipdb.set_trace()
+            row_ind = labels == i
+            col_ind = labels == j
+
+            edge_num = dense_adj[row_ind].transpose()[col_ind].sum()
+            print("edges between class {:d} and class {:d}: {:f}".format(i,j,edge_num))
+
 def norm_sparse(adj):#normalize a torch dense tensor for GCN, and change it into sparse.
     adj = adj + torch.eye(adj.shape[0]).to(adj)
     rowsum = torch.sum(adj,1)
@@ -95,3 +109,67 @@ def fix_imbalance_ratio(imbalance_ratio, labels_df, features_df, train_idx, val_
         # test_idx = [x - reduction for x in test_idx]
         # return new_train_X, new_train_Y, train_idx, val_idx, test_idx
     return features_df, labels_df, new_train_idX, val_idx, test_idx
+
+
+def fix_imbalance_ratio2(imbalance_ratio, labels_df, features_df, train_idx, val_idx, test_idx):
+    new_train_idX = train_idx
+    new_val_idX = val_idx
+    new_test_idX = test_idx
+    if imbalance_ratio != None:
+        train_total_count = len(train_idx)
+        labels_df = pd.DataFrame(labels_df, columns=['labels'])
+        labels = pd.DataFrame(labels_df, columns=['labels']).labels.unique()
+        temp = labels_df[labels_df.index.isin(train_idx)]
+        train_grouped_df = temp.groupby(by=['labels'])['labels']
+        train_grouped_df_counts = train_grouped_df.count()
+        train_minority_count = train_grouped_df_counts.min()
+        train_minority_label = train_grouped_df_counts.axes[0][train_grouped_df_counts.argmin()]
+        current_minority_total_ratio = train_minority_count / train_total_count
+        assert current_minority_total_ratio > imbalance_ratio, "The ratio is below the threshold"
+        rest_classes_count = train_total_count - train_minority_count
+        reduction = int(imbalance_ratio * rest_classes_count * (imbalance_ratio - 1) + train_minority_count)
+        labels_df_in_training = labels_df[labels_df.index.isin(train_idx)]
+        labels_df_minority = labels_df_in_training[labels_df_in_training['labels'] == train_minority_label]
+        minority_idx = labels_df_minority.index
+        drop_indices = np.random.choice(minority_idx, reduction, replace=False)
+        new_train_idX = [x for x in train_idx if x not in drop_indices]
+
+        temp = labels_df[labels_df.index.isin(val_idx)]
+        val_grouped_df = temp.groupby(by=['labels'])['labels']
+        val_grouped_df_counts = val_grouped_df.count()
+        val_minority_count = val_grouped_df_counts.min()
+        rest_classes_count = len(val_idx) - val_minority_count
+        reduction = int(imbalance_ratio * rest_classes_count * (imbalance_ratio - 1) + val_minority_count)
+        labels_df_in_val = labels_df[labels_df.index.isin(val_idx)]
+        labels_df_minority = labels_df_in_val[labels_df_in_val['labels'] == train_minority_label]
+        minority_idx = labels_df_minority.index
+        drop_indices = np.random.choice(minority_idx, reduction, replace=False)
+        new_val_idX = [x for x in train_idx if x not in drop_indices]
+
+        temp = labels_df[labels_df.index.isin(test_idx)]
+        test_grouped_df = temp.groupby(by=['labels'])['labels']
+        test_grouped_df_counts = test_grouped_df.count()
+        test_minority_count = test_grouped_df_counts.min()
+        rest_classes_count = len(test_idx) - test_minority_count
+        reduction = int(imbalance_ratio * rest_classes_count * (imbalance_ratio - 1) + test_minority_count)
+        labels_df_in_training = labels_df[labels_df.index.isin(test_idx)]
+        labels_df_minority = labels_df_in_training[labels_df_in_training['labels'] == train_minority_label]
+        minority_idx = labels_df_minority.index
+        drop_indices = np.random.choice(minority_idx, reduction, replace=False)
+        new_test_idX = [x for x in train_idx if x not in drop_indices]
+        # train_grouped_df
+        # n_classes = len(train_grouped_df)
+        # concated_train = np.concatenate([train_X, train_Y[:, None]], axis=1)
+        # col_num = concated_train.shape[-1]
+        # train_minority_label = labels[train_minority_index]
+        # grouped_con_df = pd.DataFrame(pd.DataFrame(concated_train).groupby(by=[col_num - 1]))
+        # print(labels_df)
+        # minority_ndarray = grouped_con_df.iloc[train_minority_index, 1]
+        # droped_idxs = minority_idx[minority_idx.isin(drop_indices)]
+        # new_train_X = train_X.drop(drop_indices)
+        # new_train_Y = train_Y.drop(drop_indices)
+        # train_idx = train_idx[: -reduction]
+        # val_idx = [x - reduction for x in val_idx]
+        # test_idx = [x - reduction for x in test_idx]
+        # return new_train_X, new_train_Y, train_idx, val_idx, test_idx
+    return features_df, labels_df, new_train_idX, new_val_idX, new_test_idX
