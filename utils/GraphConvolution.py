aa7c4ef7a5dd010fae25873f736e9f6ae211e7af
amozhdehi <amojdehi@gmail.com>
2022-11-20 19:26:47 -0700

    Finished initizaliation

--- utils/GraphConvolution.py
+++ utils/GraphConvolution.py
@@ -166,7 +166,7 @@ class GraphConvolution(Module):
     Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
     """
 
-    def __init__(self, in_features, out_features, bias=True):
+    def __init__(self, in_features, out_features, bias = True, init = None):
         super(GraphConvolution, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
@@ -175,11 +175,20 @@ class GraphConvolution(Module):
             self.bias = Parameter(torch.DoubleTensor(out_features))
         else:
             self.register_parameter('bias', None)
-        self.reset_parameters()
+        self.reset_parameters(init)
 
-    def reset_parameters(self):
+    def reset_parameters(self, init):
         stdv = 1. / math.sqrt(self.weight.size(1))
-        self.weight.data.uniform_(-stdv, stdv)
+        if init == "Xavier Uniform":
+            nn.init.xavier_uniform_(self.weight.data)
+        elif init == "Xavier Normal":
+            nn.init.xavier_normal_(self.weight.data)
+        elif init == "Kaiming Uniform":
+            nn.init.kaiming_uniform_(self.weight.data)
+        elif init == "Kaiming Normal":
+            nn.init.kaiming_normal_(self.weight.data)
+        else:
+            self.weight.data.uniform_(-stdv, stdv)
         if self.bias is not None:
             self.bias.data.uniform_(-stdv, stdv)
 
@@ -196,52 +205,92 @@ class GraphConvolution(Module):
 
 
 class GCN_Encoder_s(nn.Module):
-    def __init__(self, nfeat, nhid, nembed, dropout):
+    def __init__(self, nfeat, nhid, nembed, dropout, init = None):
         super(GCN_Encoder_s, self).__init__()
 
-        self.gc1 = GraphConvolution(nfeat, nhid)
+        self.gc1 = GraphConvolution(nfeat, nhid, init = init)
         self.dropout = dropout
 
-    def forward(self, x, adj):
-        x = F.relu(self.gc1(x, adj))
+    def forward(self, x, adj, funct):
+        if funct == "ReLU":
+            func = F.relu
+        elif funct == "LeakyReLU":
+            func = F.leaky_relu
+        elif funct == "Sigmoid":
+            func = sigmoid
+        elif funct == "Sigmoid":
+            func = nn.Sigmoid()
+        elif funct == "PReLU":
+            func = nn.PReLU()
+        x = func(self.gc1(x, adj))
         x = F.dropout(x, self.dropout, training=self.training)
 
         return x
 
 class GCN_Classifier_s(nn.Module):
-    def __init__(self, nembed, nhid, nclass, dropout, device):
+    def __init__(self, nembed, nhid, nclass, dropout, device, init = None):
         super(GCN_Classifier_s, self).__init__()
 
-        self.gc1 = GraphConvolution(nembed, nhid)
+        self.gc1 = GraphConvolution(nembed, nhid, init = init)
         self.mlp = nn.Linear(nhid, nclass, device = device).double()
         self.dropout = dropout
 
-        self.reset_parameters()
+        self.reset_parameters(init)
 
-    def reset_parameters(self):
-        nn.init.normal_(self.mlp.weight,std=0.05)
+    def reset_parameters(self, init):
+        if init == "Xavier Uniform":
+            nn.init.xavier_uniform_(self.mlp.weight.data)
+            # nn.init.xavier_uniform_(self.bias.data)
+        elif init == "Xavier Normal":
+            nn.init.xavier_normal_(self.mlp.weight.data)
+        elif init == "Kaiming Uniform":
+            nn.init.kaiming_uniform_(self.mlp.weight.data)
+        elif init == "Kaiming Normal":
+            nn.init.kaiming_normal_(self.mlp.weight.data)
+        else:
+            nn.init.normal_(self.mlp.weight,std=0.05)
 
-    def forward(self, x, adj):
-        x = F.relu(self.gc1(x, adj))
-        x = F.dropout(x, self.dropout, training=self.training)
-        x = self.mlp(x)
+    def forward(self, x, adj, funct):
+        if funct == "ReLU":
+            func = F.relu
+        elif funct == "LeakyReLU":
+            func = F.leaky_relu
+        elif funct == "Sigmoid":
+            func = sigmoid
+        elif funct == "Sigmoid":
+            func = nn.Sigmoid()
+        elif funct == "PReLU":
+            func = nn.PReLU()
+        x = func(self.gc1(x, adj)).double()
+        x = F.dropout(x, self.dropout, training=self.training).double()
+        x = self.mlp(x).double()
 
         return x
 
 class GCN_Encoder_w(nn.Module):
-    def __init__(self, nfeat, nclass, nhid, nembed, dropout, device):
+    def __init__(self, nfeat, nclass, nhid, nembed, dropout, device, func, init = None):
         super(GCN_Encoder_w, self).__init__()
 
-        self.gc1 = GraphConvolution(nfeat, nhid)
+        self.gc1 = GraphConvolution(nfeat, nhid, init = init)
         self.dropout = dropout
-        self.gc2 = GraphConvolution(nembed, nhid)
+        self.gc2 = GraphConvolution(nembed, nhid, init = init)
         self.mlp = nn.Linear(nhid, nclass, device = device).double()
-    def forward(self, x, adj):
-        x = F.relu(self.gc1(x, adj))
-        x = F.dropout(x, self.dropout, training=self.training)
-        x = F.relu(self.gc2(x, adj))
-        x = F.dropout(x, self.dropout, training=self.training)
-        x = self.mlp(x)
+    def forward(self, x, adj, funct):
+        if funct == "ReLU":
+            func = F.relu
+        elif funct == "LeakyReLU":
+            func = F.leaky_relu
+        elif funct == "Sigmoid":
+            func = sigmoid
+        elif funct == "Sigmoid":
+            func = nn.Sigmoid()
+        elif funct == "PReLU":
+            func = nn.PReLU()
+        x = func(self.gc1(x, adj)).double()
+        x = F.dropout(x, self.dropout, training=self.training).double()
+        x = func(self.gc2(x, adj)).double()
+        x = F.dropout(x, self.dropout, training=self.training).double()
+        x = self.mlp(x).double()
         return x
 
 class Decoder_s(Module):
@@ -249,18 +298,31 @@ class Decoder_s(Module):
     Simple Graphsage layer
     """
 
-    def __init__(self, nembed, dropout=0.1):
+    def __init__(self, nembed, dropout=0.1, init = None):
         super(Decoder_s, self).__init__()
         self.dropout = dropout
 
         self.de_weight = Parameter(torch.FloatTensor(nembed, nembed))
+        # if init == "Xavier":
+        #     nn.init.kaiming_uniform_(self.weight.data)
+        # else:
+        
+        self.reset_parameters(init)
 
-        self.reset_parameters()
 
-
-    def reset_parameters(self):
-        stdv = 1. / math.sqrt(self.de_weight.size(1))
-        self.de_weight.data.uniform_(-stdv, stdv)
+    def reset_parameters(self, init):
+        if init == "Xavier Uniform":
+            nn.init.xavier_uniform_(self.de_weight.data)
+            # nn.init.xavier_uniform_(self.bias.data)
+        elif init == "Xavier Normal":
+            nn.init.xavier_normal_(self.de_weight.data)
+        elif init == "Kaiming Uniform":
+            nn.init.kaiming_uniform_(self.de_weight.data)
+        elif init == "Kaiming Normal":
+            nn.init.kaiming_normal_(self.de_weight.data)
+        else:
+            stdv = 1. / math.sqrt(self.de_weight.size(1))
+            self.de_weight.data.uniform_(-stdv, stdv)
 
 
     def forward(self, node_embed):
@@ -272,85 +334,135 @@ class Decoder_s(Module):
 
 
 class GCN_Encoder(nn.Module):
-    def __init__(self, nfeat, nhid, nembed, dropout):
+    def __init__(self, nfeat, nhid, nembed, dropout, init = None):
         super(GCN_Encoder, self).__init__()
 
-        self.gc1 = GraphConvolution(nfeat, nhid)
+        self.gc1 = GraphConvolution(nfeat, nhid, init = init)
         self.dropout = dropout
 
-    def forward(self, x, adj, func=F.relu):
-        x = func(self.gc1(x, adj))
-        x = F.dropout(x, self.dropout, training=self.training)
+    def forward(self, x, adj, funct):
+        if funct == "ReLU":
+            func = F.relu
+        elif funct == "LeakyReLU":
+            func = F.leaky_relu
+        elif funct == "Sigmoid":
+            func = sigmoid
+        elif funct == "Sigmoid":
+            func = nn.Sigmoid().double()
+        elif funct == "PReLU":
+            func = nn.PReLU().double()
+        x = func(self.gc1(x, adj)).double()
+        x = F.dropout(x, self.dropout, training=self.training).double()
 
         return x
 
 class GCN_Encoder2(nn.Module):
-    def __init__(self, nfeat, nhid, nembed, dropout):
+    def __init__(self, nfeat, nhid, nembed, dropout, init = None):
         super(GCN_Encoder2, self).__init__()
 
-        self.gc1 = GraphConvolution(nfeat, nhid)
-        self.gc2 = GraphConvolution(nhid, nembed)
+        self.gc1 = GraphConvolution(nfeat, nhid, init = init)
+        self.gc2 = GraphConvolution(nhid, nembed, init = init)
         self.dropout = dropout
 
-    def forward(self, x, adj, func=F.relu):
-        x = func(self.gc1(x, adj))
-        x = F.dropout(x, self.dropout, training=self.training)
-        x = func(self.gc2(x, adj))
-        x = F.dropout(x, self.dropout, training=self.training)
+    def forward(self, x, adj, funct):
+        if funct == "ReLU":
+            func = F.relu
+        elif funct == "LeakyReLU":
+            func = F.leaky_relu
+        elif funct == "Sigmoid":
+            func = sigmoid
+        elif funct == "Sigmoid":
+            func = nn.Sigmoid().double()
+        elif funct == "PReLU":
+            func = nn.PReLU().double()
+        x = func(self.gc1(x, adj)).double()
+        x = F.dropout(x, self.dropout, training=self.training).double()
+        x = func(self.gc2(x, adj)).double()
+        x = F.dropout(x, self.dropout, training=self.training).double()
         return x
 
 class GCN_Encoder3(nn.Module):
-    def __init__(self, nfeat, nhid, nembed, dropout, nclass, order):
+    def __init__(self, nfeat, nhid, nembed, dropout, nclass, order, init = None):
         super(GCN_Encoder3, self).__init__()
 
         layers = []
         if len(nhid) == 0:
             # layers.append(GraphConvolution(nfeat, nclass, order=order))
-            layers.append(GraphConvolution(nfeat, nclass))
+            layers.append(GraphConvolution(nfeat, nclass, init = init))
         else:
             # layers.append(GraphConvolution(nfeat, nhid[0], order=order))
-            layers.append(GraphConvolution(nfeat, nhid[0]))
+            layers.append(GraphConvolution(nfeat, nhid[0], init = init))
             for i in range(len(nhid) - 1):
                 # layers.append(GraphConvolution(nhid[i], nhid[i + 1], order=order))
-                layers.append(GraphConvolution(nhid[i], nhid[i + 1]))
+                layers.append(GraphConvolution(nhid[i], nhid[i + 1], init = init))
         if nclass > 1:
-            layers.append(GraphConvolution(nhid[-1], nembed))
+            layers.append(GraphConvolution(nhid[-1], nembed, init = init))
             # layers.append(GraphConvolution(nhid[-1], nclass, order=order))
         self.gc = nn.ModuleList(layers)
 
         self.dropout = dropout
         self.nclass = nclass
 
-    def forward(self, x, adj, func=F.relu):
+    def forward(self, x, adj, funct):
+        if funct == "ReLU":
+            func = F.relu
+        elif funct == "LeakyReLU":
+            func = F.leaky_relu
+        elif funct == "Sigmoid":
+            func = sigmoid
+        elif funct == "Sigmoid":
+            func = nn.Sigmoid().double()
+        elif funct == "PReLU":
+            func = nn.PReLU().double()
         end_layer = len(self.gc) - 1 if self.nclass > 1 else len(self.gc)
         for i in range(end_layer):
-            x = F.dropout(x, self.dropout, training=self.training)
-            x = self.gc[i](x, adj)
-            x = func(x)
+            x = F.dropout(x, self.dropout, training=self.training).double()
+            x = self.gc[i](x, adj).double()
+            x = func(x).double()
         return x
 
 class GCN_Classifier(nn.Module):
-    def __init__(self, nembed, nhid, nclass, dropout, device):
+    def __init__(self, nembed, nhid, nclass, dropout, device, init = None):
         super(GCN_Classifier, self).__init__()
         self.device = device
-        self.gc1 = GraphConvolution(nembed, nhid)
+        self.gc1 = GraphConvolution(nembed, nhid, init = init)
         self.mlp = nn.Linear(nhid, nclass, device=device).double()
         # self.mlp = nn.Linear(nhid, nclass, device=device)
         self.dropout = dropout
 
-        self.reset_parameters()
+        self.reset_parameters(init)
 
-    def reset_parameters(self):
-        nn.init.normal_(self.mlp.weight,std=0.05)
+    def reset_parameters(self, init):
+        if init == "Xavier Uniform":
+            nn.init.xavier_uniform_(self.mlp.weight.data)
+            # nn.init.xavier_uniform_(self.bias.data)
+        elif init == "Xavier Normal":
+            nn.init.xavier_normal_(self.mlp.weight.data)
+        elif init == "Kaiming Uniform":
+            nn.init.kaiming_uniform_(self.mlp.weight.data)
+        elif init == "Kaiming Normal":
+            nn.init.kaiming_normal_(self.mlp.weight.data)
+        else:
+            nn.init.normal_(self.mlp.weight, std=0.05)
 
-    def forward(self, x, adj, func=F.relu):
-        x = func(self.gc1(x, adj))
-        x = F.dropout(x, self.dropout, training=self.training)
-        x = self.mlp(x)
+    def forward(self, x, adj, funct):
+        if funct == "ReLU":
+            func = F.relu
+        elif funct == "LeakyReLU":
+            func = F.leaky_relu
+        elif funct == "Sigmoid":
+            func = sigmoid
+        elif funct == "Sigmoid":
+            func = nn.Sigmoid().double()
+        elif funct == "PReLU":
+            func = nn.PReLU().double()
+        x = func(self.gc1(x, adj)).double()
+        x = F.dropout(x, self.dropout, training=self.training).double()
+        x = self.mlp(x).double()
 
         return x
 
 # Sigmoid function
-def sigmoid(mx, dim):
-    mx = torch.sigmoid(mx)
-    return F.normalize(mx, p=1, dim=dim)
+def sigmoid(mx):
+    mx = torch.sigmoid(mx).double()
+    return F.normalize(mx, p=1).double()
